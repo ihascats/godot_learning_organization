31702fe8cf75d7068be0979b402ca2146ac9ec72
ihascats <s.pavlovic.rs@gmail.com>
2023-04-17 19:25:11 +0200

    Add a player character

---
+++ scenes/character/BlueWizard.gd
@@ -0,0 +1,42 @@
+extends KinematicBody2D
+
+export (int) var speed = 100
+
+onready var fire_wand = $FireWand
+
+var velocity = Vector2()
+
+func get_input():
+	velocity = Vector2()
+	if Input.is_action_pressed("right"):
+		velocity.x += 1
+	if Input.is_action_pressed("left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("down"):
+		velocity.y += 1
+	if Input.is_action_pressed("up"):
+		velocity.y -= 1
+	velocity = velocity.normalized() * speed
+
+func _physics_process(_delta):
+	
+	fire_wand.position = get_local_mouse_position().normalized() * 16
+	fire_wand.look_at(get_global_mouse_position())
+	if fire_wand.position.x < 0:
+		fire_wand.flip_v = true
+	else:
+		fire_wand.flip_v = false
+		
+	
+	if fire_wand.position.y > 0:
+		fire_wand.z_index = 10
+	else:
+		fire_wand.z_index = 0
+		
+	
+	get_input()
+	velocity = move_and_slide(velocity)
+	if velocity != Vector2.ZERO:
+		$AnimatedSprite.play("walk")
+	else:
+		$AnimatedSprite.play("idle")
